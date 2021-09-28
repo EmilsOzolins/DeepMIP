5dd0cb817b97d55dd6f27cb46b22b259bf608028
Emīls Ozoliņš <ozolinsemils@gmail.com>
2021-09-28 17:52:44 +0300

    Data extraction from scip model

--- agents/primal.py
+++ agents/primal.py
@@ -1,18 +1,20 @@
-from typing import Any
+from functools import cached_property, lru_cache
+from typing import Any, List
 
-import ecole as ec
 import numpy as np
+import pyscipopt
 import torch
 
 import config as config
 import hyperparams as params
 from data.item_placement import ItemPlacementDataset
 from data.load_balancing import LoadBalancingDataset
+from data.mip_instance import MIPInstance
 from model.mip_network import MIPNetwork
 from utils.data_utils import InputDataHolder
 
 
-class ObservationFunction(ec.observation.NodeBipartite):  # This allows customizing information received by the solver
+class ObservationFunction:  # This allows customizing information received by the solver
 
     def __init__(self, problem):
         super(ObservationFunction, self).__init__()
@@ -29,113 +31,148 @@ class ObservationFunction(ec.observation.NodeBipartite):  # This allows customiz
         pass
 
     def extract(self, model, done):
-        m = model.as_pyscipopt()
-        variables = m.getVars(transformed=True)
+        m = model.as_pyscipopt()  # type: pyscipopt.scip.Model
 
-        # Extract the upper and lower bounds of all variables
-        lbs = np.asarray([v.getLbLocal() for v in variables])
-        ubs = np.asarray([v.getUbLocal() for v in variables])
+        variables = m.getVars(transformed=True)  # type: List[pyscipopt.scip.Variable]
+        var2id = {var.name: idx for idx, var in enumerate(variables)}
 
-        # extract features of variables and constraints
-        obs = super().extract(model, done)
-        column_features = obs.column_features  # Variables features
-        row_features = obs.row_features  # Constraints features
-        edge_features = obs.edge_features  # Variables-Constraints graph
+        constraints = m.getLPRowsData()  # type: List[pyscipopt.scip.Row]
+        constraints = [c for c in constraints if not c.isRemovable()]
+        # TODO: Filter ones where all variables are set by bounds
 
-        return {"lbs": lbs,
-                "ubs": ubs,
-                "column_features": column_features,
-                "row_features": row_features,
-                "edge_features": edge_features}
+        ip = MIPInstance()
+
+        for const in constraints:
+            c_mul = const.getVals()
+            columns = const.getCols()  # type: List[pyscipopt.scip.Column]
+            c_vars = [c.getVar() for c in columns]
+            c_var_ids = [var2id[v.name] for v in c_vars]
+
+            lhs = const.getLhs()
+            rhs = const.getRhs()
+
+            if lhs == rhs:
+                ip.equal(c_var_ids, c_mul, lhs)
+            elif lhs < rhs:
+                ip.greater_or_equal(c_var_ids, c_mul, lhs)
+            elif rhs > lhs:
+                ip.less_or_equal(c_var_ids, c_mul, rhs)
+            else:
+                raise RuntimeError("Something is wrong with left or right side values!")
+
+        sense = m.getObjectiveSense()
+        objective = m.getObjective()  # type: pyscipopt.scip.Expr
+        obj_exp = [(m.getTransformedVar(term.vartuple[0]), mul) for term, mul in objective.terms.items()]
+        obj_exp = [(var2id[var.name], mul) for var, mul in obj_exp]
+
+        objective_indices = [x for x, _ in obj_exp]
+        objective_mul = [float(m) for _, m in obj_exp]
+
+        if sense == "minimize":
+            ip.minimize_objective(objective_indices, objective_mul)
+        elif sense == "maximize":
+            ip.maximize_objective(objective_indices, objective_mul)
+        else:
+            raise RuntimeError(f"Unknown sense direction! Expected 'maximize/minimize' but found {sense}")
+
+        int_vars = [var2id[var.name] for var in variables if var.vtype() in {"BINARY", "INTEGER"}]
+        ip.integer_constraint(int_vars)
+
+        for var in variables:
+            ip.variable_lower_bound(var2id[var.name], var.getLbLocal())
+            ip.variable_lower_bound(var2id[var.name], var.getLbLocal())
+
+        return ip
 
 
-class MIPObservationHolder(InputDataHolder):
+class Instance2Holder(InputDataHolder):
 
-    def __init__(self, observations: dict, device: torch.device) -> None:
+    def __init__(self, ip: MIPInstance, device: torch.device, **other_data) -> None:
         super().__init__()
-        self._observations = observations
+        self._ip = ip
         self._device = device
+        self._other_data = other_data
 
-    @property
+    @cached_property
     def vars_const_graph(self) -> torch.sparse.Tensor:
-        graph = self._observations["edge_features"]  # TODO: It could be that we need to transpose this matrix
-        indices = np.int64(graph.indices)
-        torch_graph = torch.sparse_coo_tensor(indices, graph.values,
-                                              graph.shape, dtype=torch.float32,
+        indices = self._ip.variables_constraints_graph
+        values = self._ip.variables_constraints_values
+        size = self._ip.variables_constraints_graph_size
+
+        torch_graph = torch.sparse_coo_tensor(indices, values,
+                                              size, dtype=torch.float32,
                                               device=self._device).t()
         return torch_graph.coalesce()
 
-    @property
+    @cached_property
     def binary_vars_const_graph(self) -> torch.sparse.Tensor:
-        graph = self._observations["edge_features"]
-        torch_graph = torch.sparse_coo_tensor(np.int64(graph.indices),
-                                              torch.ones_like(graph.values), graph.shape,
+        indices = self._ip.variables_constraints_graph
+        values = self._ip.variables_constraints_values
+        size = self._ip.variables_constraints_graph_size
+
+        torch_graph = torch.sparse_coo_tensor(indices,
+                                              torch.ones_like(values), size,
                                               dtype=torch.float32, device=self._device).t()
         return torch_graph.coalesce()
 
-    @property
+    @cached_property
     def const_values(self) -> torch.Tensor:
-        bias = self._observations["row_features"][:, 0]
-        return torch.as_tensor(bias, device=self._device, dtype=torch.float32)
+        rhs = self._ip.right_values_of_constraints
+        return torch.as_tensor(rhs, device=self._device, dtype=torch.float32)
 
-    @property
+    @cached_property
     def vars_obj_graph(self) -> torch.sparse.Tensor:
-        graph = self._observations["edge_features"]
-        objective_mul = self._observations["column_features"][:, 0]
-        _, vars_count = graph.shape
-        indices_var = [x for x in range(vars_count)]
-        indices_obj = [0] * vars_count
+        indices = self._ip.variables_objective_graph
+        values = self._ip.variables_objective_graph_values
+        var_count, _ = self._ip.variables_constraints_graph_size
 
-        torch_graph = torch.sparse_coo_tensor((indices_var, indices_obj),
-                                              objective_mul, [vars_count, 1],
-                                              dtype=torch.float32, device=self._device)
-        return torch_graph.coalesce()
+        return torch.sparse_coo_tensor(indices, values, size=[var_count, 1], device=self._device).coalesce()
 
-    @property
+    @cached_property
     def const_inst_graph(self) -> torch.sparse.Tensor:
-        graph = self._observations["edge_features"]
-        const_count, _ = graph.shape
-        indices_var = [x for x in range(const_count)]
-        indices_inst = [0] * const_count
-        torch_graph = torch.sparse_coo_tensor((indices_var, indices_inst),
-                                              torch.ones([const_count]), [const_count, 1],
-                                              dtype=torch.float32, device=self._device)
+        indices = self._ip.constraints_instance_graph
+        values = self._ip.constraints_instance_graph_values
+        _, const_count = self._ip.variables_constraints_graph_size
 
-        return torch_graph.coalesce()
+        return torch.sparse_coo_tensor(indices, values, size=[const_count, 1], device=self._device).coalesce()
 
-    @property
+    @cached_property
     def vars_inst_graph(self) -> torch.sparse.Tensor:
-        graph = self._observations["edge_features"]
-        _, vars_count = graph.shape
-        indices_var = [x for x in range(vars_count)]
-        indices_inst = [0] * vars_count
-        torch_graph = torch.sparse_coo_tensor((indices_var, indices_inst), torch.ones([vars_count]),
-                                              size=[vars_count, 1], dtype=torch.float32, device=self._device)
-        return torch_graph.coalesce()
+        indices = self._ip.variables_instance_graph
+        values = self._ip.variables_instance_graph_values
+        var_count, _ = self._ip.variables_constraints_graph_size
 
-    @property
+        return torch.sparse_coo_tensor(indices, values, size=[var_count, 1], device=self._device).coalesce()
+
+    @cached_property
     def optimal_solution(self) -> torch.Tensor:
         return torch.tensor([float("nan")], device=self._device, dtype=torch.float32)
 
-    @property
+    @cached_property
     def integer_mask(self) -> torch.Tensor:
-        binary_mask = self._observations["column_features"][:, 1]
-        integer_mask = self._observations["column_features"][:, 2]
-        mask = np.logical_or(binary_mask, integer_mask)
-        return torch.as_tensor(mask, device=self._device, dtype=torch.float32)
+        var_count, const_count = self._ip.variables_constraints_graph_size
+        mask = torch.zeros([var_count], device=self._device)
+        indices = self._ip.integer_variables.to(device=self._device)
+        return torch.scatter(mask, dim=0, index=indices, value=1.0)
 
-    @property
+    @cached_property
     def objective_multipliers(self) -> torch.Tensor:
-        mul = self._observations["column_features"][:, 0]
-        return torch.as_tensor(mul, device=self._device, dtype=torch.float32)
+        var_count = self.vars_obj_graph.size(0)
 
+        if self.vars_obj_graph._nnz() == 0:
+            return torch.zeros([var_count], device=self._device)
+
+        return torch.sparse.sum(self.vars_obj_graph, dim=-1).to_dense()
+
+    @lru_cache(maxsize=None)
     def get_data(self, *keys: str) -> Any:
-        pass
+        data = [self._other_data[k] for k in keys]
+        data = [x.to(device=self._device) if isinstance(x, torch.Tensor) else x for x in data]
+
+        return data if len(data) > 1 else data[0]
 
 
 class Policy():
-    # TODO: Run on the train and validate datasets
-
     def __init__(self, problem):
         # called once for each problem benchmark
         self.rng = np.random.RandomState()
@@ -164,7 +201,7 @@ class Policy():
         self.rng = np.random.RandomState(seed)
 
     def __call__(self, action_set, observation):
-        obs_holder = MIPObservationHolder(observation, self.device)
+        obs_holder = Instance2Holder(observation, self.device)
 
         with torch.no_grad():
             outputs, logits = self.network.forward(obs_holder, self.device)
